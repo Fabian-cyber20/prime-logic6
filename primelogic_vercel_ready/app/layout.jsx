494222cd565cf62e6c7ef01ec32479f9291bdfed
Fabian-cyber20 <frasch.fabian@web.de>
2026-03-16 21:36:12 +0100

    Update layout.jsx

--- primelogic_vercel_ready/app/layout.jsx
+++ primelogic_vercel_ready/app/layout.jsx
@@ -8,7 +8,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body><footer style={{
+  marginTop: "80px",
+  padding: "20px",
+  borderTop: "1px solid #eee",
+  textAlign: "center",
+  fontSize: "14px"
+}}>
+  
+  <a href="/impressum" style={{marginRight:"20px"}}>
+    Impressum
+  </a>
+
+  <a href="/datenschutz">
+    Datenschutz
+  </a>
+
+</footer>{children}</body>
     </html>
   );
 }
