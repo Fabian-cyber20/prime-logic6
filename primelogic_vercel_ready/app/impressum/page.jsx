b2a3a75c0d26ef6a3167f855544eefd61b3f1c5e
Fabian-cyber20 <frasch.fabian@web.de>
2026-03-16 21:28:18 +0100

    Create page.jsx

---
+++ primelogic_vercel_ready/app/impressum/page.jsx
@@ -0,0 +1,46 @@
+export default function Impressum() {
+  return (
+    <main style={{maxWidth:"800px",margin:"80px auto",padding:"20px",fontFamily:"Arial"}}>
+      
+      <h1>Impressum</h1>
+
+      <h2>Angaben gemäß §5 TMG</h2>
+
+      <p>
+        PrimeLogic AI<br/>
+        Fabian Frasch<br/>
+        [Häuserweg 7]<br/>
+        [73760] [Ostfildern]<br/>
+        Deutschland
+      </p>
+
+      <h2>Kontakt</h2>
+
+      <p>
+        E-Mail: hello@prime-logic.de<br/>
+      </p>
+
+      <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+
+      <p>
+        Fabian [Frasch]<br/>
+        Adresse wie oben
+      </p>
+
+      <h2>Haftung für Inhalte</h2>
+
+      <p>
+        Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt.
+        Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
+      </p>
+
+      <h2>Haftung für Links</h2>
+
+      <p>
+        Unser Angebot enthält Links zu externen Websites Dritter,
+        auf deren Inhalte wir keinen Einfluss haben.
+      </p>
+
+    </main>
+  )
+}
